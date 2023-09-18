096272aba7d7c6a46efbfaa7c1d5ee2601b8ecd9
Murmotan <114673342+Murmotan@users.noreply.github.com>
2023-09-18 18:21:50 +0300

    Update Main.java
    ya shiz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main{
     public static void main (String args[]) {
-
+        Cpu ryzen = new Cpu;
+        string cpu = "ryzen 7";
+        int core = 8;
+        int thread = 16;
+        int memory = 48;
+        
     }
 }
