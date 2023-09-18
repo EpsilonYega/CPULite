bbe170a556b01965b0b5628d5c7e28982a426e87
murmota <92248518+murmota@users.noreply.github.com>
2023-09-18 18:34:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main (String args[]) {
-
+        Cpu ryzen = new Cpu;
+        sring cpu = "ryzen";
+        int core = 8;
+        int thread = 16;
     }
 }
